fb78804efe4ac17b809f3472173f81786b5d0206
Meowell123 <Meowelliko@proton.me>
2026-09-20 10:35:48 +0400

    Update UNINST.cmd

--- UNINST.cmd
+++ UNINST.cmd
@@ -1,18 +1,24 @@
-@echo off 
+@echo off
 if exist "%ipath%\" (
     echo The current Meowls installation path is %ipath%.
     echo Uninstalling...
-    timeout /t 3 /nobreak > NUL 
+    timeout /t 3 /nobreak > NUL
 
     rmdir /S /Q "%ipath%"
     if not exist "%ipath%\" (
         (
-           echo $p = [Environment]::GetEnvironmentVariable('Path', 'User');
-           echo $n = ($p) -Replace [Regex]::Escape(";${env:ipath}"), "";
-           echo [Environment]::SetEnvironmentVariable('Path', $n, 'User')
-
-        ) > %temp%\pathcleanup.ps1 
+           echo $p = [Environment]::GetEnvironmentVariable^('Path', 'User'^);
+           echo $n = ^($p^) -Replace [Regex]::Escape^(";%ipath%"^), "";
+           echo [Environment]::SetEnvironmentVariable^('Path', $n, 'User'^);
+           echo [Environment]::SetEnvironmentVariable^('ipath', $null, 'User'^);
+        ) > "%temp%\pathcleanup.ps1"
+        
         powershell -ExecutionPolicy Bypass -File "%temp%\pathcleanup.ps1" > NUL 2>&1
+        del "%temp%\pathcleanup.ps1" > NUL 2>&1
+        
+        if exist C:\ProgramData\Chocolatey\bin\RefreshEnv.cmd (
+           call C:\ProgramData\Chocolatey\bin\RefreshEnv.cmd
+        )
         echo Successfully uninstalled.
     ) else (
         echo Uninstallation failed.
